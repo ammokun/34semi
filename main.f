4681f09185223f4f0b75021c12f819888e90bb28
soichiro04 <so4sedna20@gmail.com>
2020-01-03 18:46:26 +0900

    main

---
+++ main.f
@@ -0,0 +1,13 @@
+        program main 
+
+                implicit none
+
+                integer :: imax=100 !cell number
+        
+                double precision :: c=1.0 ! velosity
+                double precision :: nu=0.50 ! CFL number
+                double precision :: l=1.0 ! length 
+
+                double precision, dimension(0:imax) :: u
+
+        end program main
